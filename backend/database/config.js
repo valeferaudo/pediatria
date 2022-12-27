e90f1aa5703bb56915aee43082ba452e0b4301fc
valeferaudo <valentinferaudo@gmail.com>
2022-12-27 20:16:22 -0300

    Update local conection mongodb

--- backend/database/config.js
+++ backend/database/config.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_STRING);
+    // await mongoose.connect(process.env.DB_STRING);
+    mongoose.connect('mongodb://localhost:27017/pediatria', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('DataBase is connected');
   } catch (error) {
     console.log(error);
